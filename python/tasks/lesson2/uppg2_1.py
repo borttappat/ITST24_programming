6eff579018e6ff68bf9ab175a4cf969371c0b547
SweJob <31406955+SweJob@users.noreply.github.com>
2024-09-26 08:43:16 +0200

    made all the string alterations on one line and removed unneccesary lowercasing

--- python/tasks/lesson2/uppg2_1.py
+++ python/tasks/lesson2/uppg2_1.py
@@ -11,16 +11,22 @@ gemener.
 '''
 
 name = " aNnA kaRlSsOn "
-print(name)
+# print(name)
 
-trimname = name.strip()
-print(trimname)
+# trimname = name.strip()
+# print(trimname)
 
-lowercasename = trimname.casefold()
-print(lowercasename)
+# lowercasename = trimname.casefold()
+# print(lowercasename)
 
-titlename = lowercasename.title()
-print(titlename)
+# titlename = lowercasename.title()
+# print(titlename)
 
-dashname = titlename.replace(" ", "-")
+# dashname = titlename.replace(" ", "-")
+
+# Shorten the above to a one-line solution that strips, "titelefies" and insert dashes at once.
+# No need for the lowercasing as title() changes that.
+dashname = name.strip().title().replace(" ","-")
+
+
 print(dashname)
