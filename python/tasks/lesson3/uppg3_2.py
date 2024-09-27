d39df7bef546aa1bfbf2b44cb3eebc0edcd496cb
driturr <123456+username@users.noreply.github.com>
2024-09-27 18:03:45 +0200

    Added content to uppg3_2.py

--- python/tasks/lesson3/uppg3_2.py
+++ python/tasks/lesson3/uppg3_2.py
@@ -7,4 +7,12 @@ python-skript.
 Skriv ut varje rad i filen som innehåller "Failed login attempt" (utan tomrader)
 '''
 
-
+def search_attempt(filename, word = "Failed login attempt"):
+    with open("log.txt", 'r') as file:
+        attempt = file.readlines() # Read the file line by line
+        for i, line in enumerate(attempt, start=1):
+            if word in line:
+                print("**************")               
+                print(f"The log {word} was found in line {i}.\nHere's how it looks like: \n{line.strip()}")
+                print("**************")
+search_attempt("log.txt")  # This will search for "Failed login attempt"
