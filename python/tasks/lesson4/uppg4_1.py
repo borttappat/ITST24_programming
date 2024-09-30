b658da1f7c461d4b14efe382840cba0bfa85fcdf
AK <borttappat@proton.me>
2024-09-30 11:28:56 +0200

    added tasks from lesson 4

---
+++ python/tasks/lesson4/uppg4_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+1. Multiplikationstabellen
+Skapa ett program som genererar och skriver ut en multiplikationstabell för ett tal
+som användaren matar in.
+Krav:
+● En funktion för att skapa en multiplikationstabell.
+● Programmet ska fråga användaren vilket tal som ska multipliceras och till
+vilket maxvärde.
+● En funktion för att kontrollera om den inmatade strängen bara innehåller
+siffror. Använd funktionen isdigit()
+● Skriv ut tabellen
+'''
