f0472677fbf3cc47e56cef8047565630da8f4167
AK <borttappat@proton.me>
2024-09-30 13:35:56 +0200

    start of project 1 - nmapscaner

---
+++ python/project1/nmapscanner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''
+Möjlighet att spara resultatet av skanningen till fil (.txt)
+Använd input/fil för att bestämma vilka ip-adresser som ska skannas
+Programmet ska ha en meny där användaren kan välja vad som ska göras.
+'''
+
+
