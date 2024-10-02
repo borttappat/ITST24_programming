ae6d6bc5e4c40d5c18ba3e1fe3c43c52ae7c2ce4
AK <borttappat@proton.me>
2024-10-02 23:27:09 +0200

    added project_1 related files: nmappy.py, an nmap scanning tool, shell.nix to drop into a dev shell, and an example IP list, ip_lists.txt

--- python/project1/nmapscanner.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-'''
-Möjlighet att spara resultatet av skanningen till fil (.txt)
-Använd input/fil för att bestämma vilka ip-adresser som ska skannas
-Programmet ska ha en meny där användaren kan välja vad som ska göras.
-'''
-
-
