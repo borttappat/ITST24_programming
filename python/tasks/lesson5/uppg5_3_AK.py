62ccb40b5ff95c0fc9e9d62341123e4a99146365
AK <borttappat@proton.me>
2024-10-03 23:46:53 +0200

    added tasks for lesson 5

---
+++ python/tasks/lesson5/uppg5_3_AK.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''
+Skapa en klass Person med attributen name och age.
+Använd en while-loop för att ta emot användarens namn och ålder.
+Validera att åldern är ett heltal
+Skapa och skriv ut ett Person-objekt om inputen är giltig.
+'''
---
+++ python/tasks/lesson5/uppg5_4_AK.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''
+Skapa en klass Rectangle med attributen length och width.
+Implementera metoder för att beräkna arean och omkretsen.
+Ta emot och validera input för length och width.
+Skriv ut arean och omkretsen
+'''
+
