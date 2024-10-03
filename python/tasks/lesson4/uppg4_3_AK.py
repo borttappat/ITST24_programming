6772f7447922d27f8fb51e2a420b2370340ef22b
AK <borttappat@proton.me>
2024-10-03 09:14:19 +0200

    added uppg4_3_AK.py

---
+++ python/tasks/lesson4/uppg4_3_AK.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''
+Skapa ett program som hanterar en inköpslista. Programmet ska kunna:
+● Lägga till, ta bort och visa alla varor till inköpslistan.
+● Spara inköpslistan till en textfil (användaren får välja filnamn).
+Krav:
+● Programmet ska ha en meny där användaren kan välja att lägga till varor, visa
+listan, ta bort varor, spara listan eller avsluta programmet.
+● Använd funktioner för att strukturera koden och hantera varje menyval.
+'''
+
+
