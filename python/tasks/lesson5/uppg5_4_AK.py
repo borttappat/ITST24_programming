bf88e2a5d917dfd12108a324505a0a3811a4d644
AK <borttappat@proton.me>
2024-10-04 23:56:38 +0200

    added solutions for 5_3 and 5_4

--- python/tasks/lesson5/uppg5_4_AK.py
+++ python/tasks/lesson5/uppg5_4_AK.py
@@ -6,3 +6,34 @@ Ta emot och validera input för length och width.
 Skriv ut arean och omkretsen
 '''
 
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = float(length)
+        self.width = float(width)
+
+    def area(self):
+        area_math = self.length * self.width
+        return f"the area of the rectangle is {area_math:.2f}"
+        
+    def circumference(self):
+        circumference_math = 2 * (self.length + self.width)
+        return f"the circumference of the rectangle is {circumference_math:.2f}"
+
+
+while True:
+    length = input("\nEnter rectangle length: ")
+    if not length.isdigit():
+        print(f"{input} is not a valid input")
+        continue
+
+    width= input("\nEnter rectangle width: ")
+    if not width.isdigit():
+        print(f"{width} is not a valid input")
+        continue
+    
+    rect = Rectangle(length, width)
+
+    print(rect.area())
+    print(rect.circumference())
+
+    break
