94e4a3095aaa850dd46960a545dee14637ec0a36
dritur <179262840+driturr@users.noreply.github.com>
2024-10-11 12:17:17 +0200

    Update uppg3_2.py

--- python/tasks/lesson3/uppg3_2.py
+++ python/tasks/lesson3/uppg3_2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-'''
+"""
 2. Logganalys
 Ladda ner log.txt från ITHSdistans. Lägg denna fil i samma katalog som ditt
 python-skript.
 Öppna filen och läs in den
 Skriv ut varje rad i filen som innehåller "Failed login attempt" (utan tomrader)
-''
+"""
 def search_attempt(filename, word = "Failed login attempt"):
     with open("log.txt", 'r') as file:
         attempt = file.readlines()
